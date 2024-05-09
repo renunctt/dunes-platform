10d679d39a8fcd0738dec286db21c19bc36e8716
renunctt <sultanabibollaev@gmail.com>
2024-05-10 00:02:03 +0500

    update

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -6,159 +6,183 @@ import wwwIcon from '@/assets/images/card/www.svg'
 import discordIcon from '@/assets/images/card/discord.svg'
 import telegramIcon from '@/assets/images/card/telegram.svg'
 import warningIcon from '@/assets/images/card/warning.svg'
-import bgVideo from '@/assets/videos/evermoon3.mp4'
+import bgVideo from '@/assets/videos/evermoon.mp4'
 import { ReactNode, useEffect, useRef, useState } from 'react'
 import { useInView } from 'framer-motion'
 import { Link } from 'react-router-dom'
 
 interface SectionProps {
-  children: ReactNode
+	children: ReactNode
 }
 
 function Section({ children }: SectionProps) {
-  const ref = useRef(null)
-  const [hasAnimated, setHasAnimated] = useState(false)
-  const isInView = useInView(ref, { once: true })
+	const ref = useRef(null)
+	const [hasAnimated, setHasAnimated] = useState(false)
+	const isInView = useInView(ref, { once: true })
 
-  useEffect(() => {
-    if (isInView && !hasAnimated) {
-      setHasAnimated(true)
-    }
-  }, [isInView])
+	useEffect(() => {
+		if (isInView && !hasAnimated) {
+			setHasAnimated(true)
+		}
+	}, [isInView])
 
-  return (
-    <section ref={ref}>
-      <div
-        ref={ref}
-        className={`${styles.fadeInSection} ${isInView && hasAnimated ? styles.isVisible : ''}`}>
-        {children}
-      </div>
-    </section>
-  )
+	return (
+		<section ref={ref}>
+			<div
+				ref={ref}
+				className={`${styles.fadeInSection} ${isInView && hasAnimated ? styles.isVisible : ''}`}
+			>
+				{children}
+			</div>
+		</section>
+	)
 }
 
 export const CardItem = () => {
-  const videoRef = useRef(null)
-  const [progress, setProgress] = useState(0)
-  const [isPlaying, setIsPlaying] = useState(false)
+	const videoRef = useRef(null)
+	const [progress, setProgress] = useState(0)
+	const [isPlaying, setIsPlaying] = useState(false)
+
+	useEffect(() => {
+		const video = videoRef.current
+
+		const updateProgress = () => {
+			if (video) {
+				const progress = (video.currentTime / video.duration) * 100
+				setProgress(progress)
+			}
+		}
+
+		const handlePlay = () => {
+			setIsPlaying(true)
+		}
+
+		const handlePause = () => {
+			setIsPlaying(false)
+		}
+
+		const handleEnded = () => {
+			setProgress(0) // Сброс анимации при завершении видео
+		}
+
+		video.addEventListener('timeupdate', updateProgress)
+		video.addEventListener('play', handlePlay)
+		video.addEventListener('pause', handlePause)
+		video.addEventListener('ended', handleEnded)
+
+		return () => {
+			video.removeEventListener('timeupdate', updateProgress)
+			video.removeEventListener('play', handlePlay)
+			video.removeEventListener('pause', handlePause)
+			video.removeEventListener('ended', handleEnded)
+		}
+	}, [])
 
   useEffect(() => {
-    const video = videoRef.current
+    const video = videoRef.current;
 
-    const updateProgress = () => {
+    const disableVideoControls = () => {
       if (video) {
-        const progress = (video.currentTime / video.duration) * 100
-        setProgress(progress)
+        video.removeAttribute("controls");
       }
-    }
+    };
 
-    const handlePlay = () => {
-      setIsPlaying(true)
+    if (video) {
+      video.addEventListener("playing", disableVideoControls);
     }
-
-    const handlePause = () => {
-      setIsPlaying(false)
-    }
-
-    const handleEnded = () => {
-      setProgress(0) // Сброс анимации при завершении видео
-    }
-
-    video.addEventListener('timeupdate', updateProgress)
-    video.addEventListener('play', handlePlay)
-    video.addEventListener('pause', handlePause)
-    video.addEventListener('ended', handleEnded)
 
     return () => {
-      video.removeEventListener('timeupdate', updateProgress)
-      video.removeEventListener('play', handlePlay)
-      video.removeEventListener('pause', handlePause)
-      video.removeEventListener('ended', handleEnded)
-    }
-  }, [])
+      if (video) {
+        video.removeEventListener("playing", disableVideoControls);
+      }
+    };
+  }, []);
 
-  return (
-    <div className={styles.card}>
-      <div className={styles.left}>
-        <div className={styles.top}></div>
+	return (
+		<div className={styles.card}>
+			<div className={styles.left}>
+				<div className={styles.top}></div>
 
-        <Section>
-          <div className={styles.content}>
-            <h3 className={styles.title}>
-              <img src={EvermoonIcon} alt="Evermoon" />
-              Evermoon
-            </h3>
+				<Section>
+					<div className={styles.content}>
+						<h3 className={styles.title}>
+							<img src={EvermoonIcon} alt='Evermoon' />
+							Evermoon
+						</h3>
 
-            <div className={styles.social}>
-              <a href="https://twitter.com">
-                <img src={twitterIcon} alt="twitter" />
-              </a>
-              <a href="https://google.com">
-                <img src={wwwIcon} alt="world wide web" />
-              </a>
-              <a href="https://discord.com">
-                <img src={discordIcon} alt="discord" />
-              </a>
-              <a href="https://telegram.org">
-                <img src={telegramIcon} alt="telegram" />
-              </a>
-            </div>
+						<div className={styles.social}>
+							<a href='https://twitter.com'>
+								<img src={twitterIcon} alt='twitter' />
+							</a>
+							<a href='https://google.com'>
+								<img src={wwwIcon} alt='world wide web' />
+							</a>
+							<a href='https://discord.com'>
+								<img src={discordIcon} alt='discord' />
+							</a>
+							<a href='https://telegram.org'>
+								<img src={telegramIcon} alt='telegram' />
+							</a>
+						</div>
 
-            <div className={styles.contentBox}>
-              <button className={styles.btn}>Gaming</button>
-              <button className={styles.btn}>Web3</button>
-            </div>
+						<div className={styles.contentBox}>
+							<button className={styles.btn}>Gaming</button>
+							<button className={styles.btn}>Web3</button>
+						</div>
 
-            <p className={styles.info}>Next-Gen 5v5 3-lane MOBA</p>
+						<p className={styles.info}>Next-Gen 5v5 3-lane MOBA</p>
 
-            <p className={styles.desc}>
-              Total Raise: <span>$300,000</span>
-            </p>
-            <p className={styles.desc}>
-              Registration Ends in: <span>8d 6h 50m 31s</span>
-            </p>
+						<p className={styles.desc}>
+							Total Raise: <span>$300,000</span>
+						</p>
+						<p className={styles.desc}>
+							Registration Ends in: <span>8d 6h 50m 31s</span>
+						</p>
 
-            <div className={styles.bottomBox}>
-              <Link
-                to={'/dunes-platform/project-info'}
-                className={`${styles.bottomBtn} shuffle-hover-inside`}>
-                PARTICIPATE NOW
-              </Link>
+						<div className={styles.bottomBox}>
+							<Link
+								to={'/dunes-platform/project-info'}
+								className={`${styles.bottomBtn} shuffle-hover-inside`}
+							>
+								PARTICIPATE NOW
+							</Link>
 
-              <span>
-                <img src={warningIcon} alt="warning" />
-                No KYC required to opt-in
-              </span>
-            </div>
-          </div>
-        </Section>
-      </div>
+							<span>
+								<img src={warningIcon} alt='warning' />
+								No KYC required to opt-in
+							</span>
+						</div>
+					</div>
+				</Section>
+			</div>
 
-      <div className={styles.right}>
-        <div className={styles.cardTop}></div>
-        <Section>
-          <div className={styles.progress}>
-            <div
-              className={styles.progressLine}
-              style={{
-                width: `${progress}%`,
-                height: '100%',
-                transition: isPlaying ? 'width 0.3s ease' : 'none' // Добавляем анимацию изменения ширины
-              }}></div>
-          </div>
-          <div className={styles.videoWrapper}>
-            <video
-              ref={videoRef}
-              className={styles.video}
-              autoPlay
-              loop
-              muted
-              src={bgVideo}
-            />
-            <div className={styles.triangle}></div>
-          </div>
-        </Section>
-      </div>
-    </div>
-  )
+			<div className={styles.right}>
+				<div className={styles.cardTop}></div>
+				<Section>
+					<div className={styles.progress}>
+						<div
+							className={styles.progressLine}
+							style={{
+								width: `${progress}%`,
+								height: '100%',
+								transition: isPlaying ? 'width 0.3s ease' : 'none', // Добавляем анимацию изменения ширины
+							}}
+						></div>
+					</div>
+					<div className={styles.videoWrapper}>
+						<video
+							ref={videoRef}
+							className={styles.video}
+							autoPlay
+							muted
+							loop
+							playsInline
+							src={bgVideo}
+						/>
+						<div className={styles.triangle}></div>
+					</div>
+				</Section>
+			</div>
+		</div>
+	)
 }
--- src/components/MainSection/Main.tsx
+++ src/components/MainSection/Main.tsx
@@ -1,35 +1,57 @@
 import styles from './Main.module.css'
-import bgVideo from '../../assets/videos/main-bg2.mp4'
+import bgVideo from '../../assets/videos/main-bg.mp4'
+import { useEffect, useRef } from 'react'
 
 export const Main = () => {
-  return (
-    <main className={styles.main}>
-      <div className={`${styles.mainWrapper} my-container`}>
-        <video
-          className={styles.video}
-          autoPlay 
-          loop 
-          muted 
-          src={bgVideo}
-        />
-        
-        <h1 className={styles.title}>
-          Join the most innovative <span>launchpad</span> of 2024
-        </h1>
+	const videoRef = useRef(null)
 
-        <p className={styles.desc}>
-          Platform forged from Sands of ancient desert to provide incubation to Innovations on Runes protocol
-        </p>
+	useEffect(() => {
+		const video = videoRef.current
 
-        <div className={styles.box}>
-          <button className={styles.buy}>
-            buy $DUNES
-          </button>
-          <button className={styles.get}>
-            Get started
-          </button>
-        </div>
-      </div>
-    </main>
-  )
+		const disableVideoControls = () => {
+			if (video) {
+				video.removeAttribute('controls')
+			}
+		}
+
+		if (video) {
+			video.addEventListener('playing', disableVideoControls)
+		}
+
+		return () => {
+			if (video) {
+				video.removeEventListener('playing', disableVideoControls)
+			}
+		}
+	}, [])
+
+	return (
+		<main className={styles.main}>
+			<div className={`${styles.mainWrapper} my-container`}>
+				<video
+					ref={videoRef}
+					className={styles.video}
+					autoPlay
+					muted
+					loop
+					playsInline
+					src={bgVideo}
+				/>
+
+				<h1 className={styles.title}>
+					Join the most innovative <span>launchpad</span> of 2024
+				</h1>
+
+				<p className={styles.desc}>
+					Platform forged from Sands of ancient desert to provide incubation to
+					Innovations on Runes protocol
+				</p>
+
+				<div className={styles.box}>
+					<button className={styles.buy}>buy $DUNES</button>
+					<button className={styles.get}>Get started</button>
+				</div>
+			</div>
+		</main>
+	)
 }

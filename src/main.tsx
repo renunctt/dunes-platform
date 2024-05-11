52a33e91da838ae6e7dcaa5a39c647b09425485c
Antuhrium <antyriyman@gmail.com>
2024-05-11 17:29:51 +0300

    gallery page

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import { Home } from './pages/Home.tsx'
-import { ProjectInfo } from './pages/ProjectInfo.tsx'
-import InoLaunchpad from './pages/InoLaunchpad.tsx'
-import { Swap } from './pages/Swap.tsx'
-import IdoLaunchpad from './pages/IdoLaunchpad.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { Home } from "./pages/Home.tsx";
+import { ProjectInfo } from "./pages/ProjectInfo.tsx";
+import InoLaunchpad from "./pages/InoLaunchpad.tsx";
+import { Swap } from "./pages/Swap.tsx";
+import IdoLaunchpad from "./pages/IdoLaunchpad.tsx";
+import Gallery from "./pages/Gallery.tsx";
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		children: [
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "",
+                element: <Home />,
+            },
+            {
+                path: "project-info",
+                element: <ProjectInfo />,
+            },
+            {
+                path: "swap",
+                element: <Swap />,
+            },
+            {
+                path: "ino-launchpad",
+                element: <InoLaunchpad />,
+            },
+            {
+                path: "ido-launchpad",
+                element: <IdoLaunchpad />,
+            },
 			{
-				path: '',
-				element: <Home />,
-			},
-			{
-				path: 'project-info',
-				element: <ProjectInfo />,
-			},
-			{
-				path: 'swap',
-				element: <Swap />,
-			},
-			{ 
-        path: 'ino-launchpad',
-        element: <InoLaunchpad />
-      },
-      { 
-        path: 'ido-launchpad',
-        element: <IdoLaunchpad />
-      }
-		],
-	},
-])
+                path: "gallery",
+                element: <Gallery />,
+            },
+        ],
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);

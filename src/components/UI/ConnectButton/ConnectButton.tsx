0d9165d89daac5b57e46aaeb7fe63d0b1428779e
renunctt <sultanabibollaev@gmail.com>
2024-05-11 20:21:05 +0500

    update

--- src/components/UI/ConnectButton/ConnectButton.tsx
+++ src/components/UI/ConnectButton/ConnectButton.tsx
@@ -1,10 +1,16 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import styles from './ConnectButton.module.css'
+import { useTextScramble } from '@skits/react-text-scramble'
 
 const ConnectButton: FC = ({ ...props }) => {
+	const { state, reveal } = useTextScramble('ᚠᚢᚦᚨᚱ ᛉᛊᛏᛒᛚᛜ', {
+    characters: 'CONNECT WALLET',
+    speed: 50,
+  });
+
 	return (
-		<button {...props} className={styles.connectBtn}>
-			Connect wallet
+		<button onMouseEnter={() => reveal(500, 0, 'typewriter', () => reveal(50, 0 , 'typewriter'))}  {...props} className={styles.connectBtn}>
+			{state.output}
 		</button>
 	)
 }

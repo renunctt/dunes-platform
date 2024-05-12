32b84a4bea703c79a5768f43cd9a43dea08da04e
renunctt <sultanabibollaev@gmail.com>
2024-05-12 09:45:16 +0500

    add file useScrambleText

--- src/components/UI/ConnectButton/ConnectButton.tsx
+++ src/components/UI/ConnectButton/ConnectButton.tsx
@@ -1,9 +1,18 @@
 import { FC } from 'react'
 import styles from './ConnectButton.module.css'
+import { useScrambleText } from '@/hooks/useScrambleText'
 
 const ConnectButton: FC = ({ ...props }) => {
+	const [ref, reload] = useScrambleText()
+
 	return (
-		<button {...props} className={styles.connectBtn}>
+		<button
+			onMouseEnter={reload}
+			onMouseLeave={reload}
+			ref={ref}
+			{...props}
+			className={styles.connectBtn}
+		>
 			CONNECT WALLET
 		</button>
 	)

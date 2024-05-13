8e4f40cc963fec68ba434bffafb8ce3afe3a171d
renunctt <sultanabibollaev@gmail.com>
2024-05-13 17:18:00 +0500

    update footer links

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,34 +1,42 @@
-import { Link } from 'react-router-dom'
 import Logo from '@/components/UI/Logo/Logo'
 import styles from './Footer.module.css'
 import twitterIcon from '@/assets/images/card/twitter.svg'
-import discordIcon from '@/assets/images/card/discord.svg'
+import telegramIcon2 from '@/assets/images/card/telegram2.svg'
 import telegramIcon from '@/assets/images/card/telegram.svg'
 import bigLogo from '@/assets/images/icons/big-logo.svg'
 import { useScrambleText } from '@/hooks/useScrambleText'
 
-const links1 = ['Our solutions', 'Documentation', 'Roadmap']
-const links2 = ['Home', 'Launchpad', 'Advisors']
+const links1 = [
+	{ link: 'https://dunesprotocol.io/#rec742480964', text: 'Our solutions'},
+	{ link: 'https://dunes-protocol.gitbook.io/untitled/', text: 'Documentation'},
+	{ link: 'https://dunesprotocol.io/#rec740439599', text: 'Roadmap'}
+]
 
-const FooterLink = ({ text }: { text: string }) => {
+const links2 = [
+	{ link: 'https://dunesprotocol.io/#rec742474381', text: 'Home'},
+	{ link: '/', text: 'Launchpad'},
+	{ link: 'https://dunesprotocol.io/#rec740440337', text: 'Advisors'}
+]
+
+const FooterLink = ({ text, link }: { text: string, link: string }) => {
 	const [ref, reload] = useScrambleText()
 
 	return (
-		<a ref={ref} onMouseEnter={reload} className={styles.link} href=''>
+		<a ref={ref} onMouseEnter={reload} className={styles.link} href={link}>
 			{text}
 		</a>
 	)
 }
 
-const FooterLink2 = ({ text }: { text: string }) => {
+const FooterLink2 = ({ text, link }: { text: string, link:string }) => {
 	const [ref, reload] = useScrambleText()
 
 	return (
-		<Link to={'/'} onMouseEnter={reload}>
+		<a href={link} onMouseEnter={reload}>
 			<div className={styles.itemLink}>
 				<p ref={ref}>{text}</p>
 			</div>
-		</Link>
+		</a>
 	)
 }
 
@@ -44,14 +52,14 @@ export const Footer = () => {
 				<div className={styles.content}>
 					<div className={styles.contentTop}>
 						<div className={styles.links}>
-							{links1.map(text => (
-								<FooterLink text={text} />
+							{links1.map(link => (
+								<FooterLink text={link.text} link={link.link} />
 							))}
 						</div>
 
 						<div className={styles.links}>
-							{links2.map(text => (
-								<FooterLink2 text={text} />
+							{links2.map(link => (
+								<FooterLink2 text={link.text} link={link.text} />
 							))}
 						</div>
 					</div>
@@ -60,14 +68,14 @@ export const Footer = () => {
 						<div className={styles.social}>
 							<p>Follow us</p>
 							<div className={styles.socialLinks}>
-								<a href='https://twitter.com'>
+								<a href='https://twitter.com/DunesProtocol'>
 									<img src={twitterIcon} alt='' />
 								</a>
-								<a href='https://discord.com'>
-									<img src={discordIcon} alt='' />
-								</a>
-								<a href='https://telegram.org'>
+								<a href='https://t.me/Dunesprotocol'>
 									<img src={telegramIcon} alt='' />
+								</a>
+								<a href='https://t.me/DunesprotocolCom'>
+									<img src={telegramIcon2} alt='' />
 								</a>
 							</div>
 						</div>

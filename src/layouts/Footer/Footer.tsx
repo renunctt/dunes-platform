ce5637134a2ff05ccb7e53bcd0cf1274e31bbc6a
renunctt <sultanabibollaev@gmail.com>
2024-05-13 17:29:30 +0500

    update links

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -5,26 +5,27 @@ import telegramIcon2 from '@/assets/images/card/telegram2.svg'
 import telegramIcon from '@/assets/images/card/telegram.svg'
 import bigLogo from '@/assets/images/icons/big-logo.svg'
 import { useScrambleText } from '@/hooks/useScrambleText'
+import { Link } from 'react-router-dom'
 
 const links1 = [
-	{ link: 'https://dunesprotocol.io/#rec742480964', text: 'Our solutions'},
-	{ link: 'https://dunes-protocol.gitbook.io/untitled/', text: 'Documentation'},
-	{ link: 'https://dunesprotocol.io/#rec740439599', text: 'Roadmap'}
+	{ link: '/swap', text: 'Dune Swap'},
+	{ link: '/bridge', text: 'Dune Bridge'},
+	{ link: '/gallery', text: 'Dune Gallery'}
 ]
 
 const links2 = [
-	{ link: 'https://dunesprotocol.io/#rec742474381', text: 'Home'},
-	{ link: '/', text: 'Launchpad'},
-	{ link: 'https://dunesprotocol.io/#rec740440337', text: 'Advisors'}
+	{ link: '/ido-launchpad', text: 'IDO Launchpad'},
+	{ link: '/ino-launchpad', text: 'INO Launchpad'},
+	{ link: '/staking', text: 'Staking'}
 ]
 
 const FooterLink = ({ text, link }: { text: string, link: string }) => {
 	const [ref, reload] = useScrambleText()
 
 	return (
-		<a ref={ref} onMouseEnter={reload} className={styles.link} href={link}>
+		<Link ref={ref} onMouseEnter={reload} className={styles.link} to={link}>
 			{text}
-		</a>
+		</Link>
 	)
 }
 
@@ -32,11 +33,11 @@ const FooterLink2 = ({ text, link }: { text: string, link:string }) => {
 	const [ref, reload] = useScrambleText()
 
 	return (
-		<a href={link} onMouseEnter={reload}>
+		<Link to={link} onMouseEnter={reload}>
 			<div className={styles.itemLink}>
 				<p ref={ref}>{text}</p>
 			</div>
-		</a>
+		</Link>
 	)
 }
 
@@ -59,7 +60,7 @@ export const Footer = () => {
 
 						<div className={styles.links}>
 							{links2.map(link => (
-								<FooterLink2 text={link.text} link={link.text} />
+								<FooterLink2 text={link.text} link={link.link} />
 							))}
 						</div>
 					</div>
---
+++ src/pages/NftInfo.tsx
@@ -0,0 +1,7 @@
+const NftInfo = () => {
+	return (
+		<>
+		
+		</>
+	)
+}
